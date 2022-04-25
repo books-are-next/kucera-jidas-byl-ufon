e29b25f264b5142658cf10a6bde1d32406dcb07a
jan-martinek <honza.martinek@gmail.com>
2022-04-25 13:41:48 +0000

    Deploying to docs from @ books-are-next/kucera-jidas-byl-ufon@b719d2784e5663b03b4e0a7cb3a285fe054df542 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-cad35a3';
+const CACHE = 'cache-b719d27';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
